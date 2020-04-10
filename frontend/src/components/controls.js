49ee5bbcd97206bb917d0dc0db113ff37f0f739d
Maciej Mionskowski <git@maciekmm.net>
2020-04-10 22:32:21 +0200

    [Code] Finalize prototype for FOLD rendering, closes #3

--- frontend/src/components/controls.js
+++ frontend/src/components/controls.js
@@ -1,3 +1,6 @@
+import { GuidePlay } from '../guide_play'
+import {parseFOLD} from '../fold'
+
 export default class Controls {
     constructor(element, player) {
         this._player = player
@@ -5,8 +8,16 @@ export default class Controls {
         this._play = element.querySelector("#ctrl-play")
         this._pause = element.querySelector("#ctrl-pause")
         this._stop = element.querySelector("#ctrl-stop")
-
+        
+        this._load = element.querySelector("#load-model")
+        this._modelName = element.querySelector("#model-name")
         this._attachListeners()
+    }
+
+    renderModel(model) {
+        const play = new GuidePlay(parseFOLD(model))
+        this._player.guide = play
+        this._player.play()
     }
 
     _attachListeners() {
@@ -22,7 +33,14 @@ export default class Controls {
             event.preventDefault()
             this._player.stop()            
         }.bind(this))
+
+        this._load.addEventListener('change', (event) => {
+            const fileReader = new FileReader()
+            fileReader.onload = (data) => {
+                this.renderModel(JSON.parse(data.target.result))
+            }
+            this._modelName.innerText = this._load.files[0].name
+            fileReader.readAsText(this._load.files[0])
+        })
     }
-
-
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,22 +4,6 @@ import {parseFOLD} from './fold'
 
 const player = new Player(document.body)
 
-async function renderModel(model) {
-    const play = new GuidePlay(parseFOLD(model))
-    player.guide = play
-    player.play()
-}
-
-const loadInput = document.getElementById("load-model")
-loadInput.addEventListener('change', (event) => {
-    const fileReader = new FileReader()
-    fileReader.onload = function(data) {
-        renderModel(JSON.parse(data.target.result))
-    }
-    document.querySelector("#model-name").innerText = loadInput.files[0].name
-    fileReader.readAsText(loadInput.files[0])
-})
-
 document.addEventListener('resize', event => {
-    viewer.onResize(visualizerElement.clientWidth, visualizerElement.clientHeight)
+    player.onResize(visualizerElement.clientWidth, visualizerElement.clientHeight)
 })
