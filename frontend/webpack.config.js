3d663ee790ff8971097b0f7f662eee4d0dddce35
Maciej Mionskowski <git@maciekmm.net>
2020-04-12 18:22:14 +0200

    [Code] Replace rollup with webpack, closes #16

---
+++ frontend/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.html$/,
+                use: 'html-loader'
+            },
+            {
+                test: /\.css$/,
+                exclude: /public\/style\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /public\/.*\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: 'index.html',
+            filename: 'index.html'
+        })
+    ]
+}
