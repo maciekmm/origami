f925eb332f40fbaa3e50c85020a8e50ea810a6ba
Maciej Mionskowski <git@maciekmm.net>
2020-04-13 13:38:42 +0200

    [Code] Dumbify model loader

--- frontend/src/components/model-loader/index.js
+++ frontend/src/components/model-loader/index.js
@@ -1,13 +1,13 @@
-import React from "react"
-import InsertDriveFile from "@material-ui/icons/InsertDriveFile"
-import preprocessFOLDModel from "../../fold/preprocess"
+import React from 'react'
+import InsertDriveFile from '@material-ui/icons/InsertDriveFile'
 
 export default function ModelLoader(props) {
+
 	const loadModel = (event) => {
 		const fileReader = new FileReader()
 		fileReader.onload = (data) => {
 			const foldModel = JSON.parse(data.target.result)
-			props.loadModel(preprocessFOLDModel(foldModel))
+			props.loadModel(foldModel)
 		}
 		fileReader.readAsText(event.target.files[0])
 	}
@@ -15,7 +15,7 @@ export default function ModelLoader(props) {
 	return (
 		<label>
 			<InsertDriveFile />
-			{props.model && <span>{props.model.file_author}</span>}
+			{props.name && <span>{props.name}</span>}
 			<input id="load-model" onChange={loadModel} type="file" />
 		</label>
 	)
