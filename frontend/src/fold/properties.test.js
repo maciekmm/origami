9707860f7b14e2bafc58ec093fb80152c9579edc
Maciej Mionskowski <git@maciekmm.net>
2020-08-16 11:05:11 +0200

    Add tests

--- frontend/src/fold/properties.test.js
+++ frontend/src/fold/properties.test.js
@@ -1,4 +1,4 @@
-import { FRAME_RATE_PROPERTY } from "./properties"
+import { FRAME_RATE_PROPERTY, getComputedProperty } from "./properties"
 
 describe("FRAME_RATE_PROPERTY", () => {
 	it("should be a file property", () => {
@@ -7,5 +7,46 @@ describe("FRAME_RATE_PROPERTY", () => {
 
 	it("should be in origuide namespace", () => {
 		expect(FRAME_RATE_PROPERTY).toMatch(/^[^:]*_og:/)
+	})
+})
+
+describe("getComputedProperty", () => {
+	it("should return return the frame's property if it exists", () => {
+		const frames = [{ prop: "value" }]
+
+		const value = getComputedProperty(frames, 0, "prop")
+		expect(value).toStrictEqual("value")
+	})
+
+	it("should return return undefnied if frame has no such property", () => {
+		const frames = [{}]
+
+		const value = getComputedProperty(frames, 0, "prop")
+		expect(value).toStrictEqual(undefined)
+	})
+
+	it("should return parent's property if frame has no such property, but parent does", () => {
+		const frames = [{ prop: "value" }, { frame_inherit: true, frame_parent: 0 }]
+
+		const value = getComputedProperty(frames, 1, "prop")
+		expect(value).toStrictEqual("value")
+	})
+
+	it("should reference the correct frame", () => {
+		const frames = [{ prop: "value-0" }, { prop: "value-1" }]
+
+		const value = getComputedProperty(frames, 1, "prop")
+		expect(value).toStrictEqual("value-1")
+	})
+
+	it("should return recursively search for value until found or chain ends", () => {
+		const frames = [
+			{ prop: "value" },
+			{ frame_inherit: true, frame_parent: 0 },
+			{ frame_inherit: true, frame_parent: 1 },
+		]
+
+		const value = getComputedProperty(frames, 2, "prop")
+		expect(value).toStrictEqual("value")
 	})
 })
