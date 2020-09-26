d323c87b68a76e7c9c77d2e2ca6fefa85136065d
Maciej Mionskowski <git@maciekmm.net>
2020-09-26 19:31:09 +0200

    Add processing time limit

--- backend/community/core/tasks.py
+++ backend/community/core/tasks.py
@@ -1,18 +1,18 @@
 import io
+
 from celery import shared_task
+from celery.exceptions import SoftTimeLimitExceeded
+from django.core.files.storage import default_storage
 from origuide.loader import solve_fold
 
 from core.models import Guide
-
-from django.core.files.storage import default_storage
-from django.core.files import File
 
 
 def path_to_name(path: str) -> str:
     return path.split('/')[-1]
 
 
-@shared_task
+@shared_task(ignore_result=True, time_limit=180 * 60, soft_time_limit=120 * 60)
 def process_guide(guide_pk):
     guide = Guide.objects.get(pk=guide_pk)
     guide.status = Guide.ProcessingStatus.PROCESSING
@@ -27,6 +27,9 @@ def process_guide(guide_pk):
         guide.status = Guide.ProcessingStatus.DONE
         guide.animation_file = file_path
         print(f"Processing {guide.pk} finished.")
+    except SoftTimeLimitExceeded:
+        print(f"Time limit exceeded for processing {guide.pk}")
+        guide.status = Guide.ProcessingStatus.TIMEOUT
     except Exception as e:
         print(f"Processing of {guide.pk} failed.", e)
         guide.status = Guide.ProcessingStatus.ERROR
