648cfc52832b4033ae1b7cee56c3b49efb7b9f13
Maciej Mionskowski <git@maciekmm.net>
2020-12-16 16:10:01 +0100

    [Thesis] Typesetting

--- thesis/assets/listings/solver.py
+++ thesis/assets/listings/solver.py
@@ -1,21 +1,15 @@
 for step in Instruction.steps:
     solve(step.vertices, step.target_angles)
 
-
 def solve(vertices, target_angles):
-    cur_forces = set_forces()
     set_target_angles(target_angles)
 
     while not finished:
+        cur_forces = set_forces()
+
         for (v, total_force) in zip(vertices, cur_forces):
-            node_mass = v.mass
-            v_t = v.velocity
-            p_t = v.pos
-            a = total_force / node_mass
+            v_t, p_t = v.velocity, v.pos
+            a = total_force / v.mass
             v_next = v_t + a * d_t
             v.pos = p_t + v_next * d_t
             v.velocity = v_next
-
-        prev_forces = cur_forces.copy()
-        cur_forces = set_forces()
-
